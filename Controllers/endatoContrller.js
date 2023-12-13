2ab93a3fe3926990400e33ebb9b59af8727d0b73
Welcome Sir <mahmoudsaad6675@gmail.com>
2023-12-13 22:00:55 +0200

    Fixing Airtable And Phone 4

--- Controllers/endatoContrller.js
+++ Controllers/endatoContrller.js
@@ -467,6 +467,7 @@ exports.step2final_SearchContact = async function (BusinessNames, res) {
         // setTimeout(async () => {
             let tempObj = BusinessNames[i]
             tempObj.officers = []
+            tempObj.BusinessPhones = []
             for (let x = 0; x < BusinessNames[i]["Primary Names"].length; x++) {
                 await new Promise((resolve) => setTimeout(resolve, 1000));
                 try {
@@ -514,7 +515,6 @@ exports.step2final_SearchContact = async function (BusinessNames, res) {
                                 searchBusinssRes = collect_officers_from_NewResponse(response.data)
                             }
                             console.log(searchBusinssRes);
-                            tempObj.BusinessPhones = [];
                             tempObj.BusinessPhones[0].push(searchBusinssRes.busPhones);
                             tempObj
                                 .officers
